0faa2c36f206b9d3468ea8b5a50af982abe13cd8
Vadim <va.trifonov@gmail.com>
2019-03-02 12:39:23 +1100

    Initial commit

---
+++ Constraints/Constraints.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Constraints.
+FOUNDATION_EXPORT double ConstraintsVersionNumber;
+
+//! Project version string for Constraints.
+FOUNDATION_EXPORT const unsigned char ConstraintsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Constraints/PublicHeader.h>
+
+
